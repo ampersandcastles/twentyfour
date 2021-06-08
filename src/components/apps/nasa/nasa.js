7dac1adc0f1a520941ba464bf7f30f260b11fbcc
Amy Melangton <are.daily@gmail.com>
2021-06-08 14:07:16 -0400

    amyt's branch

---
+++ src/components/apps/nasa/nasa.js
@@ -0,0 +1,31 @@
+import { useState, useEffect, useCallback } from "react";
+const Nasa = ({ lat, long }) => {
+  console.log(lat, long);
+  const [data, setData] = useState();
+  const urlCreator = window.URL || window.webkitURL;
+
+// import React from 'react';
+ const initImgSrc = async () => {
+    if (lat && long) {
+        const key = 'wYN5Dx5DUa7fm5NenrhhlcEeHGviL8bfDcB8drgs'
+        const url = `https://api.nasa.gov/planetary/earth/imagery?lon=${long}&lat=${lat}&date=2018-02-07&api_key=${key}`
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+            'Content-Type': 'application/json',
+            })})
+        const blobData = await response.blob();
+      setData(blobData);
+    }
+  };
+  useEffect(() => {
+    initImgSrc();
+  }, [lat, long]);
+  return (
+    <div>
+      
+      {data && <img width="400px" height="400px" src={urlCreator.createObjectURL(data)}/>}
+    </div>
+  );
+};
+export default Nasa;
