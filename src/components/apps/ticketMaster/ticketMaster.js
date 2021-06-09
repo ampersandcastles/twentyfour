6455ec14c4a6b02bc500806cca4fd4e32d006223
chels_s <clsextonc734@gmail.com>
2021-06-09 11:32:27 -0400

    updated return to display fetch results on screen

--- src/components/apps/ticketMaster/ticketMaster.js
+++ src/components/apps/ticketMaster/ticketMaster.js
@@ -1,51 +1,82 @@
-import React, {useState} from 'react';
-import {render} from 'react-dom';
-import {geolocated} from "react-geolocated";
-
-
+import React, {useState, useEffect} from 'react';
 
 const NearestEvents =() => {
     const [lat, setLat] = useState(null);
     const [long, setLong] = useState(null);
     const [status, setStatus] = useState(null);
+    const [data, setData]= useState([]);
 
-    const getLocation = () => {
-        if(!navigator.geolocation) {
-            setStatus('Geolocation is not supported by your browser');
-        } else {
-            setStatus('Locating...');
+    const getLocation =() => {
+        setStatus('Locating...');
             navigator.geolocation.getCurrentPosition((position) => {
                 setLat(position.coords.latitude);
                 setLong(position.coords.longitude);
-                fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=Gph5eT3UoUhietGGAxGHBYNpaG4nhv6n&latlong=${position.coords.latitude},${position.coords.longitude}`)
-                .then(function(result){
-                    console.log(result);
-                    return result.json();
-                })
-                .then(function(json) {
-                    console.log(json);
-                    
-
-                })
+                fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=Gph5eT3UoUhietGGAxGHBYNpaG4nhv6n&latlong=${position.coords.latitude},${position.coords.longitude}`) 
+                .then((res) => res.json())
+                .then((eventData) => {
+                    console.log(eventData._embedded.events);
+                    setData(eventData);
+                    // return data.map((eventData) => <li>{eventData._embedded.event}</li>);
+                }).catch(err => console.log(err))
             })
-            
+         
         }
-    }
-
-
+    
+    useEffect(()=> {
+            getLocation();
+        }, []);
 
 return(
-    <div className ="App">
-        <h1>Coordinates</h1>
-        <p>{status}</p>
-        {lat && <p>Latitude: {lat}</p>}
-        {long && <p>Latitude: {long}</p>}
-        <button onClick={getLocation}>Get Location</button>
-    </div>
-        
+   <div className ="App">
+        <h1>Events near you:</h1>
+        {lat && <p>Your Latitude: {lat}</p>}
+        {long && <p>Your Longitude: {long}</p>}
+
+        <pre>{JSON.stringify(data,null,2)}</pre>
+    
+  {/* <button onClick={getLocation}>Get Location</button>  */}
+
+        </div>
     )
+
 
 }
 
 export default NearestEvents;
 
+
+
+// function FetchApi() {
+//     const [data, setData] = useState([]);
+    
+//     const apiGet = () =>{
+//         fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=Gph5eT3UoUhietGGAxGHBYNpaG4nhv6n`)
+//         .then((response) => response.json())
+//         .then((json) => {
+//             console.log(json);
+//             setData(json);
+//         });
+//     };
+
+//     useEffect(()=> {
+//         apiGet();
+//     }, []);
+
+//     return (
+//         <div>
+//             Events: <br/>
+//             <button onClick={apiGet}>Fetch Events</button> 
+//             <br />
+//             {/* <pre>{JSON.stringify(data,null,2)}</pre> */}
+//             <div>
+//                 <ul>
+//                     {data.map((item) => (
+//                     <li>{item._embedded.events.name}</li>
+//                     ))}
+//                 </ul>
+//                 </div>
+//             </div>
+//     )
+
+    
+// }
